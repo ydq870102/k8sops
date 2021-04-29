ad217a59ff2a454252a0a1a1f2b6859119fa01d1
yangdq01 <yangdq01@haid.com.cn>
2021-04-29 10:35:16 +0800

    init

---
+++ tls/tls.sh
@@ -0,0 +1,2 @@
+kubectl create secret tls tls-xxx --cert=cert/405011.pem --key=cert/405011.key -n common-system
+kubectl create secret tls tls-xxx --cert=cert/405011.pem --key=cert/405011.key -n mip-system
